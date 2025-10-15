35aefe0010abf04c1c8172b0f46c802f9ae8ccb0
Rom1xa <abakumoff.roman2018@yandex.ru>
2025-10-15 21:25:50 +0300

    add lab3 and lab4

---
+++ lab3/main.c
@@ -0,0 +1,118 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+static volatile sig_atomic_t signal_received = 0;
+
+void cleanup_handler(void) {
+  printf("--- [atexit handler]: Процесс с PID %d завершает работу. ---\n",
+         getpid());
+}
+
+void sigint_handler(int signum) {
+  (void)signum;
+  char msg[] =
+      "--- [signal handler]: Получен сигнал SIGINT (Ctrl+C). Игнорирую. ---\n";
+  write(STDOUT_FILENO, msg, sizeof(msg) - 1);
+}
+
+void sigterm_handler(int signum, siginfo_t *info, void *context) {
+  (void)context;
+  (void)info;
+  char msg[120];
+  sprintf(msg,
+          "--- [sigaction handler]: Получен сигнал SIGTERM (%d). Завершаю "
+          "работу. ---\n",
+          signum);
+  write(STDOUT_FILENO, msg, strlen(msg));
+  exit(1);
+}
+
+int main() {
+  printf("Родительский процесс запущен с PID: %d, PPID: %d\n", getpid(),
+         getppid());
+
+  if (atexit(cleanup_handler) != 0) {
+    perror("Ошибка регистрации atexit()");
+    exit(1);
+  }
+
+  if (signal(SIGINT, sigint_handler) == SIG_ERR) {
+    perror("Ошибка установки обработчика SIGINT");
+    exit(1);
+  }
+
+  struct sigaction sa;
+  memset(&sa, 0, sizeof(sa));
+  sa.sa_sigaction = sigterm_handler;
+  sa.sa_flags = SA_SIGINFO;
+  sigemptyset(&sa.sa_mask);
+
+  if (sigaction(SIGTERM, &sa, NULL) == -1) {
+    perror("Ошибка установки обработчика SIGTERM");
+    exit(1);
+  }
+
+  printf("Обработчики сигналов установлены. Для тестирования используйте:\n");
+  printf("- Ctrl+C для SIGINT\n");
+  printf("- kill -TERM %d для SIGTERM\n", getpid());
+
+  FILE *f = fopen("pid.txt", "w");
+  if (f) {
+    fprintf(f, "%d", getpid());
+    fclose(f);
+    printf("PID записан в файл pid.txt\n");
+  }
+
+  printf("\nВызываем fork()...\n");
+
+  pid_t child_pid = fork();
+
+  if (child_pid == -1) {
+    perror("Ошибка fork()");
+    exit(1);
+  } else if (child_pid == 0) {
+    printf("Это дочерний процесс.\n");
+    printf("Дочерний процесс: PID = %d, PPID = %d\n", getpid(), getppid());
+    printf("Дочерний процесс: засыпаю на 5 секунд...\n");
+    sleep(5);
+    printf("Дочерний процесс: завершаю работу с кодом 42.\n");
+    exit(42);
+  } else {
+    printf("Это родительский процесс.\n");
+    printf("Родительский процесс: PID = %d, PPID = %d\n", getpid(), getppid());
+    printf("Создан дочерний процесс с PID: %d\n", child_pid);
+
+    int status;
+    printf("Родительский процесс: ожидаю завершения дочернего процесса...\n");
+    pid_t waited_pid = wait(&status);
+
+    if (waited_pid == -1) {
+      perror("Ошибка wait()");
+      exit(1);
+    }
+
+    if (WIFEXITED(status)) {
+      int exit_code = WEXITSTATUS(status);
+      printf("Родительский процесс: дочерний процесс завершился с кодом %d.\n",
+             exit_code);
+    } else if (WIFSIGNALED(status)) {
+      int signal_num = WTERMSIG(status);
+      printf(
+          "Родительский процесс: дочерний процесс был завершен сигналом %d.\n",
+          signal_num);
+    }
+
+    printf("Родительский процесс: засыпаю на 10 секунд...\n");
+    sleep(10);
+    printf("Родительский процесс завершает работу\n");
+  }
+
+  printf("Процесс %d завершается нормально\n", getpid());
+
+  return 0;
+}
---
+++ lab4/main.c
@@ -0,0 +1,151 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <errno.h>
+
+void print_usage(const char *program_name) {
+    printf("Использование: %s РЕЖИМ ФАЙЛ...\n", program_name);
+}
+
+int parse_numeric_mode(const char *mode_str) {
+    char *endptr;
+    long mode = strtol(mode_str, &endptr, 8);
+    
+    if (*endptr != '\0' || mode < 0 || mode > 0777) {
+        return -1;
+    }
+    
+    return (int)mode;
+}
+
+int parse_symbolic_mode(const char *mode_str, mode_t current_mode) {
+    mode_t new_mode = current_mode;
+    const char *p = mode_str;
+    
+    mode_t who = 0;
+    mode_t what = 0;
+    char op = 0;
+    
+    if (*p == '+' || *p == '-' || *p == '=') {
+        who = S_IRWXU | S_IRWXG | S_IRWXO;
+    } else {
+        while (*p && *p != '+' && *p != '-' && *p != '=') {
+            switch (*p) {
+                case 'u': who |= S_IRWXU; break;
+                case 'g': who |= S_IRWXG; break;
+                case 'o': who |= S_IRWXO; break;
+                case 'a': who |= S_IRWXU | S_IRWXG | S_IRWXO; break;
+                default:
+                    return -1;
+            }
+            p++;
+        }
+    }
+    
+    if (*p == '\0') return -1;
+    
+    op = *p++;
+    
+    while (*p && *p != '\0') {
+        switch (*p) {
+            case 'r': 
+                if (who & S_IRWXU) what |= S_IRUSR;
+                if (who & S_IRWXG) what |= S_IRGRP;
+                if (who & S_IRWXO) what |= S_IROTH;
+                break;
+            case 'w': 
+                if (who & S_IRWXU) what |= S_IWUSR;
+                if (who & S_IRWXG) what |= S_IWGRP;
+                if (who & S_IRWXO) what |= S_IWOTH;
+                break;
+            case 'x': 
+                if (who & S_IRWXU) what |= S_IXUSR;
+                if (who & S_IRWXG) what |= S_IXGRP;
+                if (who & S_IRWXO) what |= S_IXOTH;
+                break;
+            default:
+                return -1;
+        }
+        p++;
+    }
+    
+    switch (op) {
+        case '+':
+            new_mode |= what;
+            break;
+        case '-':
+            new_mode &= ~what;
+            break;
+        case '=':
+            if (who & S_IRWXU) new_mode &= ~S_IRWXU;
+            if (who & S_IRWXG) new_mode &= ~S_IRWXG;
+            if (who & S_IRWXO) new_mode &= ~S_IRWXO;
+            new_mode |= what;
+            break;
+        default:
+            return -1;
+    }
+    
+    return new_mode;
+}
+
+mode_t get_file_mode(const char *filename) {
+    struct stat st;
+    if (stat(filename, &st) == -1) {
+        return 0;
+    }
+    return st.st_mode & 0777;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        print_usage(argv[0]);
+        return 1;
+    }
+    
+    const char *mode_str = argv[1];
+    int new_mode = -1;
+    
+    if (mode_str[0] >= '0' && mode_str[0] <= '7') {
+        new_mode = parse_numeric_mode(mode_str);
+        if (new_mode == -1) {
+            fprintf(stderr, "mychmod: неверный числовой режим '%s'\n", mode_str);
+            return 1;
+        }
+    } else {
+        for (int i = 2; i < argc; i++) {
+            mode_t current_mode = get_file_mode(argv[i]);
+            if (current_mode == 0) {
+                fprintf(stderr, "mychmod: не удалось получить права файла '%s': %s\n", 
+                        argv[i], strerror(errno));
+                continue;
+            }
+            
+            new_mode = parse_symbolic_mode(mode_str, current_mode);
+            if (new_mode == -1) {
+                fprintf(stderr, "mychmod: неверный символьный режим '%s'\n", mode_str);
+                return 1;
+            }
+            
+            if (chmod(argv[i], new_mode) == -1) {
+                fprintf(stderr, "mychmod: не удалось изменить права файла '%s': %s\n", 
+                        argv[i], strerror(errno));
+                return 1;
+            }
+        }
+        return 0;
+    }
+    
+    for (int i = 2; i < argc; i++) {
+        if (chmod(argv[i], new_mode) == -1) {
+            fprintf(stderr, "mychmod: не удалось изменить права файла '%s': %s\n", 
+                    argv[i], strerror(errno));
+            return 1;
+        }
+    }
+    
+    return 0;
+}
